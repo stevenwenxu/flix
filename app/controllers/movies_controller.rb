c33b3a243ba6c2da1aa18ece9a4c9ef65eb6d45f
Steven Xu <steven.xu@shopify.com>
2021-01-04 17:11:52 -0500

    step 13

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,27 @@ class MoviesController < ApplicationController
 	def edit
 		@movie = Movie.find(params[:id])
 	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		# https://api.rubyonrails.org/classes/ActionController/Parameters.html#method-i-permit
+		@movie.update(movie_params)
+		redirect_to @movie
+	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		new_movie = Movie.new(movie_params)
+		new_movie.save
+		redirect_to new_movie
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+	end
 end
