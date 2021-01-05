c4376ce571b124d581656d7162561f3c69c9867d
Steven Xu <steven.xu@shopify.com>
2021-01-05 16:44:29 -0500

    step 17

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -37,6 +37,6 @@ class MoviesController < ApplicationController
 	private
 
 	def movie_params
-		params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+		params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_214923) do
+ActiveRecord::Schema.define(version: 2021_01_05_213242) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_12_28_214923) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
 
 end
