816222909b7c86801bdc5dd1dc32e4c2405aabe3
Steven Xu <steven.xu@shopify.com>
2021-01-05 16:07:01 -0500

    step 16

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+
+	# all the movies that have been released, ordered with the most recently-released movie first
+	def self.released
+		where('released_on < ?', Time.now).order(released_on: :desc)
+	end
+
 	def flop?
 		total_gross.blank? || total_gross < 255_000_000
 	end
