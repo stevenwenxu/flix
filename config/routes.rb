3218ec7182d315f41c2b9c131b968e47b3a122e8
Steven Xu <steven.xu@shopify.com>
2021-01-06 12:16:05 -0500

    step 21

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # get "movies/:id" => "movies#show", as: :movie
   # get "movies/:id/edit" => "movies#edit", as: :edit_movie
   # patch "movies/:id" => "movies#update"
+
+  resources :reviews
 end
