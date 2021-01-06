28bfc2f7185040f0a0d787a3d7c75c2a159c386f
Steven Xu <steven.xu@shopify.com>
2021-01-06 09:53:55 -0500

    step 18

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,16 @@
 class Movie < ApplicationRecord
 
+	RATINGS = %w(G PG PG-13 R NC-17)
+
+	validates :title, :released_on, :duration, presence: true
+	validates :description, length: { maximum: 25 }
+	validates :total_gross, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validates :image_file_name, format: {
+		with: /\w+\.(jpg|png)\z/i,
+		message: "must be a JPG or PNG image"
+	}
+	validates :rating, inclusion: { in: RATINGS }
+
 	# all the movies that have been released, ordered with the most recently-released movie first
 	def self.released
 		where('released_on < ?', Time.now).order(released_on: :desc)
