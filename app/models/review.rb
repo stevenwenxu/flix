6e7a2b500908c46f0490558011cee14de69c28b9
Steven Xu <steven.xu@shopify.com>
2021-01-06 15:55:03 -0500

    step 23

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
   validates :name, presence: true
   validates :comment, length: { minimum: 4 }
   validates :stars, inclusion: 1..5
+
+  def stars_as_percent
+  	(stars / 5.0) * 100.0
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
 
   root "movies#index"
 
-  resources :movies
-  # get "movies" => "movies#index"
-  # get "movies/:id" => "movies#show", as: :movie
-  # get "movies/:id/edit" => "movies#edit", as: :edit_movie
-  # patch "movies/:id" => "movies#update"
-
-  resources :reviews
+  resources :movies do
+	resources :reviews
+  end
 end
