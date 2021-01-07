4ad50a04f53c308bf0b3b390280d8148fe6aadd7
Steven Xu <steven.xu@shopify.com>
2021-01-07 11:07:35 -0500

    step 24

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,11 @@
 class Review < ApplicationRecord
+  STARS = 1..5
+
   belongs_to :movie
 
   validates :name, presence: true
   validates :comment, length: { minimum: 4 }
-  validates :stars, inclusion: 1..5
+  validates :stars, inclusion: STARS
 
   def stars_as_percent
   	(stars / 5.0) * 100.0
