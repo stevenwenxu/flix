b9a3c0b84fa51e23625c280c80f4235ac6809128
Steven Xu <steven.xu@shopify.com>
2021-01-09 21:06:43 -0500

    step 28

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
 	# allow_blank useful for editing user profiles
 	validates :password, length: { minimum: 3, allow_blank: true }
-
+	validates :username, presence: true, format: { with: /\A[A-Z0-9]+\z/i} , uniqueness: true
 	def gravatar_id
 		Digest::MD5::hexdigest(email.downcase)
 	end
