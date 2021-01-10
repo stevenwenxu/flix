b45a788ffa68bf7b0c9c8c16df21dac35eee0c8b
Steven Xu <steven.xu@shopify.com>
2021-01-10 13:01:49 -0500

    step 30

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+	user = User.find_by(email: params[:email_or_username]) ||
+          User.find_by(username: params[:email_or_username])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to user, notice: "Welcome, #{user.name}!"
+  	else
+  		flash.now[:alert] = "Incorrect email/password"
+  		render :new
+  	end
   end
 
   def destroy
