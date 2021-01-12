986ae96cb9c259e9fe51c2a3c4b643283735486f
Steven Xu <steven.xu@shopify.com>
2021-01-11 22:17:08 -0500

    step 33.1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+	private
+
+	# this is equivalent to putting it as a view helper. This method is now accessible from both controllers and views.
+	helper_method :current_user
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def require_signin
+		unless @current_user
+			redirect_to signin_path, alert: "Please sign in first!"
+		end
+	end
 end
