f612d01d81461431de01c07a0fc2c7439bb98ec0
Steven Xu <steven.xu13@gmail.com>
2022-11-30 22:03:21 -0500

    Fix ujs in rails 7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Entry point for the build script in your package.json
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("./channels")
